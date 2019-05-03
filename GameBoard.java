12fd26ec32625b79a8e5473aa0a0d7eefd577bfc
FabianCMI <45429131+FabianCMI@users.noreply.github.com>
2019-05-03 16:29:51 +0200

    square

--- GameBoard.java
+++ GameBoard.java
@@ -10,12 +10,32 @@ public class GameBoard {
 	// ---------- Attributs ----------
 
 	/** Tab game board */
-	Tiles[][] gameBoardTab = new Tiles[15][15];
+	Square[][] grid = new Square[15][15];
 
 	// ---------- Constructors ----------
 
+	/**
+	 * gameboard's constructor
+	 */
 	GameBoard() {
 
 	}
+
+	// ---------- Methods ----------
+
+	/**
+	 * Calcul the score of words 
+	 * 
+	 * @param firstLetter The square where the first letter of the word is
+	 * @param wordLength The number of letter in the word
+	 * @return The score of the word
+	 */
+    public int wordScoreCalcul(Square firstLetter, int wordLength){
+        if(this.typeScoreMult){
+			//for
+            this.grid[firstLetter.x][firstLetter.y].tileScoreCalcul();
+        }
+        return(this.tile.getValue()*this.scoreMultiplicator);
+    }
 
 }
---
+++ Square.java
@@ -0,0 +1,89 @@
+/**
+ * Scrabble (APO Project - CMI L1) GameBoard.java - Represents the squares of the gameboard's
+ * grid
+ * 
+ * @author Fabian Devel, Valentin Perignon
+ */
+
+public class Square {
+// ---------- Attributs ----------
+
+	/** The tile on the square */
+    private Tiles tile;
+    /** The score multiplicator of the square */
+    private int scoreMultiplicator;
+    /** The score multiplicator's type */
+    private boolean typeScoreMult;
+    /** the abscissa of the square */
+    private int x;
+    /** the ordinate of the square */
+    private int y;
+     
+
+	// ---------- Constructors ----------
+
+    /**
+	 * Square constructor
+	 * 
+	 * @param tile The tile on the square
+	 * @param scoreMultiplicator  The score multiplicator of the square (values 1, 2 or 3)
+     * @param typeScoreMult The score multiplicator's type (false for letter, true for entire word)
+	 */
+	Square(Tiles tile, int scoreMultiplicator, boolean typeScoreMult, int x, int y) {
+        this.tile = tile;
+        this.scoreMultiplicator = scoreMultiplicator;
+        this.typeScoreMult = typeScoreMult;
+        this.x = x;
+        this.y = y;
+    }
+    
+    // ---------- Methods ----------
+
+    /**
+	 * Get the tile
+	 * 
+	 * @return The tile on the square
+	 */
+    public Tiles getTile(){
+        return(this.tile);
+    }
+
+    /**
+	 * Get the abscissa of the square 
+	 * 
+	 * @return The abscissa 
+	 */
+    public int getAbscissa(){
+        return(this.x);
+    }
+
+    /**
+	 * Get the ordinate of the square 
+	 * 
+	 * @return The ordinate
+	 */
+    public int getOrdinate(){
+        return(this.y);
+    }
+
+    /**
+	 * Set the tile
+	 * 
+	 * @param tile The tile to put on the square
+	 */
+    public void setTile(Tiles tile){
+        this.tile = tile;
+    }
+
+    /**
+	 * Calcul the score of the square 
+	 * 
+	 * @return The score of the square
+	 */
+    public int tileScoreCalcul(){
+        if(this.typeScoreMult){
+            return(this.tile.getValue());
+        }
+        return(this.tile.getValue()*this.scoreMultiplicator);
+    }
+}
