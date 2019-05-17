6648123521ce54fc2770eb694a9db6306bbf6324
Valentin P <valentin.perignon@orange.fr>
2019-05-17 08:46:34 +0200

    Normalement, NORMALEMENT HEIN, la saisie des coordonnées fonctionne

--- Library.java
+++ Library.java
@@ -49,6 +49,67 @@ public class Library {
 	}
 
 	/**
+	 * Put a String into an array of int
+	 * 
+	 * @param tabInt   The array of integers
+	 * @param str      The String
+	 * @param splitter The splitter (e.g. ",", "", ...)
+	 */
+	public static void  charstoArray(int[] tabInt, String str, String splitter) {
+		// Variable
+		String[] tabStr = new String[tabInt.length];
+		char[] tabChar = new char[tabInt.length];
+
+		// Split the array
+		tabStr = str.split(splitter);
+
+		// Array of char
+		for(int i=0; i<tabChar.length; i++)
+			tabChar[i] = tabStr[i].charAt(0);
+
+		// String to int
+		for (int i=0; i<tabInt.length; i++)
+			tabInt[i] = charToInt(tabChar[i]);
+	}
+
+	private static int charToInt(char letter) {
+		// Variables
+		int number;
+		letter = Character.toUpperCase(letter);
+
+
+		// Treatment
+		switch(letter) {
+			case 'A':
+				number = 1;
+				break;
+			case 'B':
+				number = 2;
+				break;
+			case 'C':
+				number = 3;
+				break;
+			case 'D':
+				number = 4;
+				break;
+			case 'E':
+				number = 5;
+				break;
+			case 'F':
+				number = 6;
+				break;
+			case 'G':
+				number = 7;
+				break;
+			default:
+				number = 0;
+				break;
+		}
+
+		return number;
+	}
+
+	/**
 	 * Check if the values of an array are in [min, max]
 	 */
 	public static boolean checkValuesOfArray(int[] tab, int min, int max) {
